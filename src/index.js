890be60c1f0b0f250a343abee959bffdb67d5375
Huy Dang Tran - areto consulting <huy.tran@areto.de>
2022-05-06 11:42:52 +0200

    finished challenged

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 //1. Create a new React app.
+import React from "react"
+import ReactDom from "react-dom"
 //2. Create a App.jsx component.
+import App from "./component/app.jsx"
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
@@ -8,5 +11,9 @@
 //<h1> for a title and a <p> for the content.
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
-
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+ReactDom.render(
+    <div>
+        <App />
+    </div>,
+    document.getElementById("root"))
